bf56bc33e02b0b8194e5847634e7e6bcb7bccd83
Charles Pura <164540352+charlespura@users.noreply.github.com>
2026-03-25 00:52:51 +0800

    1st commit

--- update_resident.php
+++ update_resident.php
@@ -8,6 +8,7 @@ $name = $conn->real_escape_string($_POST['name']);
 $address = $conn->real_escape_string($_POST['address']);
 $map_link = $conn->real_escape_string($_POST['map_link']);
 
+
 $conn->query("UPDATE residents SET name='$name', address='$address', map_link='$map_link' WHERE id=$id");
 
 header("Location: index.php");
